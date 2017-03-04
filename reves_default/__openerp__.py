ad7cca181f7f6e7595d071f7698313d017c6accd
jobiols <jorge.obiols@gmail.com>
2017-03-04 17:10:56 -0300

    [ADD] agregar voucher_payment_check_fix al manifiesto

--- reves_default/__openerp__.py
+++ reves_default/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#-----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
 #
 #    Copyright (C) 2016  jeo Software  (http://www.jeo-soft.com.ar)
 #    All Rights Reserved.
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#-----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
 
 {
     'name': 'Ramos Revestimientos',
@@ -31,35 +31,35 @@ Customización Ramos Revestimientos
 """,
     'author': 'jeo Software',
     'depends': [
-        'l10n_ar_base',             # modulo base para localización argentina
-        'base_vat_unique',          # evita que duplique cuit
-        #'base_vat_unique_parent',  # evita que duplique cuit en multicompañia
-        'disable_openerp_online',   # elimina referencias a odoo online
+        'l10n_ar_base',  # modulo base para localización argentina
+        'base_vat_unique',  # evita que duplique cuit
+        # 'base_vat_unique_parent',  # evita que duplique cuit en multicompañia
+        'disable_openerp_online',  # elimina referencias a odoo online
         'account_cancel',  # Muestra el check en los diarios que permite cancelar asientos
-        'product_pricelist_import', # Importa lista de precios y carga productos
-        'hide_product_variants',    # oculta las variantes
-        'im_chat',                  # mensajeria instantanea entre usuarios de odoo
-#        'express_checkout',         # Facturación express
-        'invoice_order_by_id',       # ordena facturas ultima arriba
-#        'sale_order_recalculate_prices',  # agrega boton para recalcular precios
-#        'consult_product_price',    # consulta de precios
-#        'partner_search',            # permite buscar partners por varios criterios
-        #'account_journal_sequence'  # agrega un campo de secuencia en el diario para elegirlos
-        #'account_statement_move_import'  # agrega boton de importar aputnes en extractos bancarios
-#        'account_journal_sequence', #Adds sequence field on account journal and it is going to be considered when choosing journals in differents models.
-        'l10n_ar_aeroo_sale',       # dependencia requerida
-        'l10n_ar_aeroo_purchase',   # dependencia requerida
-        'l10n_ar_aeroo_einvoice',   # dependencia requerida
-        'l10n_ar_aeroo_stock',      # dependencia requerida
-#        'po_custom_reports',        # dependencia requerida
-#        'custom_vat_ledger',        # dependencia requerida
-#        'odoo_argentina_fix',       # patch a la localización
-#        'account_invoice_tax_add',  # agrega insercion manual de impuestos para factura de compras
-#        'ticket_citi_fix',          # corrige citi para pv impresor fiscal
-         'product_unique_default_code', # impide que se duplique el default_code
-         'hide_messaging',  # oculta el menu de mensajeria
-         'base_multi_store',  # agrega capacidad de multitienda analogo a multicompañia
-         'voucher_payment_check_fix', #evita que aparezca cheques propios en medios de pago de cliente
+        'product_pricelist_import',  # Importa lista de precios y carga productos
+        'hide_product_variants',  # oculta las variantes
+        'im_chat',  # mensajeria instantanea entre usuarios de odoo
+        #        'express_checkout',         # Facturación express
+        'invoice_order_by_id',  # ordena facturas ultima arriba
+        #        'sale_order_recalculate_prices',  # agrega boton para recalcular precios
+        #        'consult_product_price',    # consulta de precios
+        #        'partner_search',            # permite buscar partners por varios criterios
+        # 'account_journal_sequence'  # agrega un campo de secuencia en el diario para elegirlos
+        # 'account_statement_move_import'  # agrega boton de importar aputnes en extractos bancarios
+        #        'account_journal_sequence', #Adds sequence field on account journal and it is going to be considered when choosing journals in differents models.
+        'l10n_ar_aeroo_sale',  # dependencia requerida
+        'l10n_ar_aeroo_purchase',  # dependencia requerida
+        'l10n_ar_aeroo_einvoice',  # dependencia requerida
+        'l10n_ar_aeroo_stock',  # dependencia requerida
+        #        'po_custom_reports',        # dependencia requerida
+        #        'custom_vat_ledger',        # dependencia requerida
+        #        'odoo_argentina_fix',       # patch a la localización
+        #        'account_invoice_tax_add',  # agrega insercion manual de impuestos para factura de compras
+        #        'ticket_citi_fix',          # corrige citi para pv impresor fiscal
+        'product_unique_default_code',  # impide que se duplique el default_code
+        'hide_messaging',  # oculta el menu de mensajeria
+        'base_multi_store',  # agrega capacidad de multitienda analogo a multicompañia
+        'voucher_payment_check_fix',  # evita que aparezca cheques propios en medios de pago de cliente
     ],
     'data': [
         'security/security_groups.xml',
