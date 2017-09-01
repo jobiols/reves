1fe3203d45ccfb79ad76ed1afae910cf4210335a
jobiols <jorge.obiols@gmail.com>
2017-09-01 19:35:26 -0300

    [FIX]  version: 8.0.1.1
    - Revisado remitos en envios y en retiros

--- reves_default/__openerp__.py
+++ reves_default/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Ramos Revestimientos',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.1',
     'category': 'Tools',
     'summary': 'Customización Ramos Revestimientos',
     'description': """
